f2d4b4c08a0fc4203f628f2db58171bff76385d1
Your Name <you@example.com>
2024-11-23 13:48:19 +0200

    eroooooo

---
+++ Assets/c##.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PrintName : MonoBehaviour
+{
+    void Start()
+    {
+   //  int num=10;
+
+
+        string message ="I'm sorry for the delay";
+        string BB ="I'm sorry for the delay";
+        
+    Debug.Log(message==BB);
+//    Debug.Log(num+55);
+//    Debug.LogError("Side motor not working");
+//    Debug. LogWarning("There is a heavy load");
+    }
+}
